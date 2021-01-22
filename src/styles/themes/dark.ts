c3926325e90a0b0a14e9e56896ead394b2e755a8
lucasblackdev <lucaasferreirafl@gmail.com>
2021-01-22 15:59:27 -0300

    feat: add initial setup

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,14 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#333",
+    secundary: "#5c2ec6",
+
+    background: "#222",
+    text: "#fff",
+    textSecundary: "#FFF",
+  },
+
+  fonts: {},
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,14 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#5c2ec6",
+    secundary: "#FFF",
+
+    background: "#F5F5F5",
+    text: "#333",
+    textSecundary: "#FFF",
+  },
+
+  fonts: {},
+};
